258e93a6b85c47d8af1610f45bc3ec7419fdf89e
charlie <charliechenzx@gmail.com>
2019-09-26 21:35:20 +0800

    Formal release

--- Readme.md
+++ Readme.md
@@ -1,21 +1,42 @@
+# Content
+- [Content](#content)
+- [Introduction](#introduction)
+- [Relating Website](#relating-website)
+- [Requirement](#requirement)
+  - [Python: 3.*](#python-3)
+  - [Module:](#module)
+- [Program Instructions](#program-instructions)
+  - [Batch mode](#batch-mode)
+  - [Live mode](#live-mode)
+- [Quizlet Import Instructions](#quizlet-import-instructions)
 # Introduction
 Find the definitions of given words and generate a txt file with **Chinese** translation, which can be loaded into Quizlet  
 There are two input modes: **batch mode and live mode** (specified in instruction)
 
 # Relating Website
-youdao.com
+youdao.com  
+
+Visit youdao.com to get Chinese translations
 
 # Requirement
-Python: 3.4+  
-Module: 
+## Python: 3.*  
+## Module: 
 - requests
 - lxml.tree
+- time.time
+- tqdm.tqdm
 
-# Instructions
-Open the program and choose a mode (1 for batch mode, 2 for live mode) 
-
-Input the vacabulary words in either Batch mode and Live mode
+# Program Instructions
+1. Open the program and choose a mode (1 for batch mode, 2 for live mode) 
+2. Input the vacabulary words in either Batch mode and Live mode
 ## Batch mode
-Input words in a txt file (./txt/input.txt), words separated by enter (\n). You need to create the file at the path before choosing the path.
+Input words in a txt file (./txt/input.txt), words separated by enter (\n). You need to create the file at the path before choosing the mode.
 ## Live mode
 Input words interactiviely. The program will return the Chinese and append result the result file.
+
+# Quizlet Import Instructions
+1. Click "Create" at the home page
+2. Click "Import from Word, Excel, Google Docs, etc."
+3. Copy and paste content in the output.txt into the frame.
+4. Click "Import"
+5. Follow the prompt to add languages and click "Import" again.
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from lxml import etree
+import time
+from tqdm import tqdm
 
 DICURL="http://youdao.com/w/eng/"
 INURL="./txt/input.txt"
@@ -7,6 +9,9 @@ OUTURL="./txt/output.txt"
 vocab={}
 
 def search(word):
+    if word=="":
+        print("No input is detected, try to input again")
+        return ""
     url=DICURL+word
     res=requests.get(url)
     htm=res.text
@@ -15,46 +20,86 @@ def search(word):
     ret=""
     if ans==[]:
         print("cannot find the definition of the word "+word)
-        return ret
+        return ""
     for itm in ans:
         ret+=itm.split("；")[0]+" "
     return ret
 
-def engine():
+def txtMode():
+    print("="*52)
+    print("|  Welcome to TxtMode. Make sure you have created  |")
+    print("|  a txt file in the path './txt/input.txt'\t   |")
+    print("="*52)
     with open(INURL,"r") as f:
-        for i in f:
+        voc=f.readlines()
+        print("|  There are totally "+str(len(voc))+" vocabulary words \t   |")
+        print("="*52)
+        for i in tqdm(voc):
             if(i=="\n" or i==""): continue
             i=i.strip("\n")
             vocab[i]=search(i)
+
     with open(OUTURL,"w") as f:
         for i in vocab:
-            wr=i+" "+vocab[i]+"\n"
+            wr=i+"\t"+vocab[i]+"\n"
             f.write(wr)
-    print("done. Program ends")
+    print("done. Program ends in 3 seconds...")
+    time.sleep(3)
 
-def live():
+def liveMode():
     try:
+        print("="*73)
+        print("|    Welcome to LiveMode. In here, you can type any vocabulary word.\t|")
+        print("|    They will be saved into the output txt unless you type in 'b' \t|")
+        print("|    and 'entr' to cancel it. You can type 'z' and 'etr' to exit\t\t|")
+        print("="*73)
+        word=input("Please input a vocab word: ")
+        las_word=word
         while 1:
-            word=input("please input a vocab word: ")
+            
             defin=search(word)
-            if defin=="": 
-                print()
-                continue
-            print(defin)
-            with open(OUTURL,"a") as f:
-                f.write(word+" "+defin+"\n")
+            if defin=="":   #no result is gained, denoting error in input
+                pass
+            else:
+                print(defin)
             print()
-    except:
-        print("Program ends")
+            
+            word=input("Please input a vocab word: ")
+            if word=="b":
+                print("Cancel the saving process\n")
+                word=input("Please input a vocab word: ")
+                las_word=word
+                continue
+            elif word=="z":
+                with open(OUTURL,"a") as f:
+                    f.write(las_word+"\t"+defin+"\n")
+                exit()
+
+            if defin!="":
+                with open(OUTURL,"a") as f:
+                    f.write(las_word+"\t"+defin+"\n")
+            las_word=word
+
+            
+    except SystemExit:
+        print ("All work is saved.\nBye~")
+    else:
+        print("Error occurs. Program ends in 3 seconds... ")
+        time.sleep(3)
 
 def main():
-    mod=input("please input a mode (1 for txt mode; 2 for live mode): ")
+    print("="*33)
+    print("|  Welcome!\t\t\t|")
+    print("|  Author: Charlie\t\t|")
+    print("|  Email: hi@smscharlie.com\t|")
+    print("="*33)
+    mod=0
+    while mod!="1" and mod!="2":
+        mod=input("Please input a mode (1 for txt mode; 2 for live mode): ")
     if mod=="1":
-        engine()
+        txtMode()
     elif mod=="2":
-        live()
-    else:
-        print("input error. Program terminetes")
+        liveMode()
 
 if __name__=="__main__":
     main()
